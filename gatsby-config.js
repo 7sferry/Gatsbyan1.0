27590b93e50a59adc30eb5971d64ac009e66b1a5
MR Ferry <ferry.suhandri@pji-solutions.com>
2020-07-04 20:48:42 +0700

    feat: improve font

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,15 +95,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `Raleway`,
-          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
-        ],
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: siteConfig.author.realName,
